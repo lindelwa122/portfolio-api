8ec23da015ce77a0c50cd828d08e5feeaaca472d
Lindelwa <lindelwa.codes@gmail.com>
2024-03-12 07:53:45 +0200

    Add a controller to save a draft.

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -10,3 +10,62 @@ exports.create_draft = asyncHandler(async (req, res, next) => {
     blog_id: blog._id,
   });
 });
+
+const validateDraft = () => {
+  return [
+    body('title')
+    .trim()
+    .isLength({ min: 3, max: 50 })
+    .exists()
+    .escape(),
+
+    body('content')
+    .trim()
+    .isLength({ min: 50 })
+    .exists()
+    .escape()
+  ];
+}
+
+const createError = (next, msg, status) => {
+  const err = new Error(msg);
+  err.status = status ?? 400;
+  return next(err);
+}
+
+exports.save_draft = [
+  validateDraft(),
+
+  asyncHandler(async (req, res, next) => {
+    if (!req.params.id) {
+      return createError(
+        next,
+        'The id parameter is required!'
+      )
+    }
+
+    const blog = new Blog({
+      title: req.body.title,
+      content: req.body.content,
+      last_saved: Date.now(),
+    });
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return createError(
+        next,
+        'The body has invalid information!'
+      );
+    } else {
+      const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {});
+
+      if (updatedBlog) {
+        res.status(200).json({
+          message: 'Blog updated successfully!',
+        });
+      } else {
+        return createError(next, "Blog wasn't updated!", 500);
+      }
+    }
+  })
+]
