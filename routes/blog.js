db73732db15895ab0dae28dd5f5badb89a603ba0
Lindelwa <lindelwa.codes@gmail.com>
2024-03-13 13:21:14 +0200

    Add blog routes.

---
+++ routes/blog.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router;
+
+const blog_controller = require('../controllers/blog-controller');
+
+router.post('/create-draft', blog_controller.create_draft);
+router.post('/save-draft/:id', blog_controller.save_draft);
+router.post('/publish', blog_controller.publish);
+
+module.exports = router;
