5d58f8796f45d263d0a3f6846a3d447b3fde8153
Lindelwa <lindelwa.codes@gmail.com>
2024-03-16 14:19:50 +0200

    Add a route to get a specified blog.

--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const blog_controller = require('../controllers/blog-controller');
 
 router.get('/drafts', blog_controller.get_all_drafts);
+router.get('/:id', blog_controller.get_blog);
 router.post('/create-draft', blog_controller.create_draft);
 router.post('/save-draft/:id', blog_controller.save_draft);
 router.post('/publish/:id', blog_controller.publish);
