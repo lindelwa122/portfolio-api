fabc50576170fd60cb96d55e120d5a29ceb051bb
Lindelwa <lindelwa.codes@gmail.com>
2024-03-16 13:32:14 +0200

    Add a blog serializer.

---
+++ serializers/blog-serializer.js
@@ -0,0 +1,35 @@
+class BlogSerializer {
+  constructor(Blog) {
+    this.returnValue = {
+      title: Blog.title,
+      content: Blog.content,
+      drafted_on: Blog.drafted_on,
+      published_on: Blog.published_on,
+      state: Blog.state,
+      last_saved: Blog.last_saved,
+      stats: Blog.stats,
+    };
+  }
+
+  exclude (keys) {
+    const keysArr = keys.split(' ');
+    for (const key of keysArr) {
+      delete this.returnValue[key];
+    }
+  }
+
+  include (keys) {
+    const keysArr = keys.split(' ');
+    for (const [key, _] of Object.entries(this.returnValue)) {
+      if (!keysArr.includes(key)) {
+        delete this.returnValue[key];
+      }
+    }
+  }
+
+  getJSON () {
+    return this.returnValue;
+  }
+}
+
+module.exports = BlogSerializer;
